0f85492ea2715f5a778d4df2d24ec895594dcee9
likeshan168 <likeshan168@163.com>
2017-11-09 19:54:18 +0800

    uploading files application by spring and gradle

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,18 +1,25 @@
 package hello;
 
+import hello.storage.StorageProperties;
+import hello.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+@EnableConfigurationProperties(StorageProperties.class)
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Application.class);
+    @Bean
+    CommandLineRunner init(StorageService storageService){
+        return (args)->{
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
---
+++ src/main/java/hello/storage/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package hello.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message){
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause){
+        super(message,cause);
+    }
+}
